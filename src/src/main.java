441d64bdb9d676f6ff11abda955cebb0dd2b4986
jerome <jerome@DESKTOP-LL5H85S>
2024-10-22 14:18:08 +0200

    ajout grille + add affichage grille dans le main (grille simple)

--- src/src/main.java
+++ src/src/main.java
@@ -1,10 +1,24 @@
 package src;
 
+import Systeme.Grille;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		Grille g1 = new Grille(20,20);
+		g1.afficher();
 	}
 
 }
